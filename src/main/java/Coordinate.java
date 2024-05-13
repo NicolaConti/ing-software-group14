6e5c82d24b1ab525c15302816e90cef1c5e0542b
NicolaConti <continicolaa@gmail.com>
2024-05-13 15:35:17 +0200

    Started work on Java classes

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -1,2 +1,9 @@
 public class Coordinate {
+    public String latitude;
+    public String longitude;
+
+    public Coordinate(String lat, String lon) {
+        this.latitude = lat;
+        this.longitude = lon;
+    }
 }
--- src/main/java/RegisteredUser.java
+++ src/main/java/RegisteredUser.java
@@ -1,2 +1,8 @@
 public class RegisteredUser extends User{
+    public String password;
+
+    public RegisteredUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
