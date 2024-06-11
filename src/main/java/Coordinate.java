0e1d4830a25cfd8d5215c65e19340b4fb54c217c
NicolaConti <continicolaa@gmail.com>
2024-06-11 16:01:22 +0200

    Finalising work on the admin part, added http calls for suspending users, retrieving suspended users list, list of segnalazioni and closing a segnalazione. Further testing in the master branch will be needed.

--- src/main/java/Coordinate.java
+++
@@ -1,9 +0,0 @@
-public class Coordinate {
-    public String latitude;
-    public String longitude;
-
-    public Coordinate(String lat, String lon) {
-        this.latitude = lat;
-        this.longitude = lon;
-    }
-}
--- src/main/java/RegisteredUser.java
+++
@@ -1,8 +0,0 @@
-public class RegisteredUser extends User{
-    public String password;
-
-    public RegisteredUser(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-}
