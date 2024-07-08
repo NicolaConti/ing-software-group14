fcec1b53fe7e0f8f272266d08c4b46290ad21dae
antodila <antonio02120212@gmail.com>
2024-07-08 12:53:07 +0200

    changes
    Signed-off-by: antodila <antonio02120212@gmail.com>

---
+++ src/main/models/segnalazioniController.js
@@ -0,0 +1,71 @@
+const Segnalazione = require('./Segnalazione');
+const Counter = require('./counter');
+
+async function getNextSequence(name) {
+    const counter = await Counter.findByIdAndUpdate(
+        { _id: name },
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+    );
+    return counter.seq;
+}
+
+exports.creaSegnalazione = async (req, res) => {
+    try {
+        const { tipo, commento, data, coordinate } = req.body;
+        const nextId = await getNextSequence('segnalazioneid');
+
+        const nuovaSegnalazione = new Segnalazione({
+            _id: nextId,
+            tipo: tipo,
+            commento: commento,
+            data: data,
+            coordinate: coordinate
+        });
+
+        const segnalazioneSalvata = await nuovaSegnalazione.save();
+
+        res.status(201).json(segnalazioneSalvata);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Errore durante la creazione della segnalazione' });
+    }
+};
+
+exports.aggiungiCommento = async (req, res) => {
+    try {
+        const segnalazione = await Segnalazione.findById(req.params.idSegnalazione);
+
+        if (!segnalazione) {
+            return res.status(404).json({ error: 'Segnalazione non trovata' });
+        }
+
+        const nuovoCommento = {
+            username: req.body.username,
+            commento: req.body.commento,
+            reportId: req.params.idSegnalazione
+        };
+
+        segnalazione.feedbacks.push(nuovoCommento);
+
+        const segnalazioneAggiornata = await segnalazione.save();
+
+        res.json(segnalazioneAggiornata);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Errore durante l\'aggiunta del commento' });
+    }
+};
+
+exports.ottieniCommenti = async (req, res) => {
+    try {
+        const segnalazione = await Segnalazione.findById(req.params.idSegnalazione);
+        if (!segnalazione) {
+            return res.status(404).json({ error: 'Segnalazione non trovata' });
+        }
+        res.json(segnalazione.feedbacks);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Errore durante il recupero dei commenti' });
+    }
+};
