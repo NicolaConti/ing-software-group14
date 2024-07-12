1ba6fcbe20b619cf738f9a123296790ba76f5da0
arbjo <arbjoguza@yahoo.it>
2024-07-12 22:06:22 +0200

    test filtri segnalazioni

--- src/main/models/Segnalazione.js
+++ src/main/models/Segnalazione.js
@@ -5,23 +5,13 @@ const segnalazioneSchema = new mongoose.Schema({
     tipo: String,
     commento: String,
     data: Date,
+    gravity: Number, // 1 to 5
     coordinate: [Number],
     feedbacks: [{
         username: String,
         commento: String
     }]
 }, {collection: 'Segnalazioni'});
-
-const Segnalazione = mongoose.model('Segnalazioni', segnalazioneSchema);
-
-const SegnalazioneSchema = new mongoose.Schema({
-    id: Number,
-    tipo: String,
-    commento: String,
-    coordinate: [Number],
-    gravity: Number, // 1 to 5
-    feedbacks: [String]
-});
 
 const Segnalazione = mongoose.model('Segnalazione', SegnalazioneSchema);
 
