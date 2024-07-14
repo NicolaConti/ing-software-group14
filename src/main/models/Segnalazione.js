dcbf233f230be46a5cae2398d983136382967c53
antodila <antonio02120212@gmail.com>
2024-07-14 18:47:15 +0200

    username
    Signed-off-by: antodila <antonio02120212@gmail.com>

--- src/main/models/Segnalazione.js
+++ src/main/models/Segnalazione.js
@@ -7,6 +7,7 @@ const feedbackSchema = new mongoose.Schema({
 
 const segnalazioneSchema = new mongoose.Schema({
     id: Number,
+    username: String,
     tipo: String,
     commento: String,
     data: Date,
